e16c6060b09f2809fe56ec0ef23e5a5984b6b270
Ellen Ambrose <ambrose.ellen@gmail.com>
2016-03-27 17:58:45 -0400

    changed bokeh plot sizes for background page

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def poverty_health():
   vals_Y2 = regr2[0]*np.float64(vals_X2) + regr2[1]
   plot2.line(vals_X2,vals_Y2,color='red',line_width=1.5)
 
-  p=gridplot([[plot],[plot2]])
+  p=gridplot([[plot,plot2]])
   script, div = components(p,CDN)
   script=script.strip()
   return render_template('myplot.html', page_title=page_title, script=script, div=div)
@@ -69,7 +69,8 @@ def vendor_availability():
             xlabel='Poverty Rate by Percentile',
             ylabel='Stores per 1000 People',
             xgrid=False,title='Change in food vendors as poverty rate increases - US counties, 2012',
-color=['Aqua','CornflowerBlue','Blue','BlueViolet','Coral','Crimson','Green','HotPink'])
+color=['Aqua','CornflowerBlue','Blue','BlueViolet','Coral','Crimson','Green','HotPink'],
+          plot_width=750, plot_height=400)
 
   script, div = components(p,CDN)
   return render_template('myplot.html', page_title=page_title, script=script, div=div)
@@ -88,7 +89,7 @@ def predict_obesity():
   highci_svm = np.percentile(scores_svm,97.5,axis=0)
   CIs_svm = np.matrix([lowci_svm,highci_svm])
 
-  p1 = figure(title='Predicting obesity from poverty, food access, food availability (logistic)',title_text_font_size='12pt')
+  p1 = figure(title='Predicting obesity from poverty, food access, food availability (logistic)',title_text_font_size='12pt',plot_width=500, plot_height=400)
   p1.quad(top=.7,bottom=.5,left=[.55,1.55,2.55,3.55],right=[1.45,2.45,3.45,4.45],alpha=.2,color=['red','blue','blue','blue'])
   p1.multi_line(xs=[[1,1],[2,2],[3,3],[4,4]],ys=[CIs[:,0],CIs[:,1],CIs[:,2],CIs[:,3]],line_width = 4)
   p1.circle(x=[1,2,3,4],y=scores.mean(axis=0),size=10,color='red')
@@ -101,7 +102,7 @@ def predict_obesity():
   p1.text(2.7,.51,['Drop access'],text_font_size='10pt')
   p1.text(3.6,.51,['Drop availablity'],text_font_size='10pt')
 
-  p2 = figure(title='Predict obesity from poverty, food access, food availability (SVM)',title_text_font_size = '12pt')
+  p2 = figure(title='Predict obesity from poverty, food access, food availability (SVM)',title_text_font_size = '12pt',plot_width=500,plot_height=400)
   p2.quad(top=.7,bottom=.5,left=[.55,1.55,2.55,3.55],right=[1.45,2.45,3.45,4.45],alpha=.2,color=['red','blue','blue','blue'])
   p2.multi_line(xs=[[1,1],[2,2],[3,3],[4,4]],ys=[CIs_svm[:,0],CIs_svm[:,1],CIs_svm[:,2],CIs_svm[:,3]],line_width=4)
   p2.circle(x=[1,2,3,4],y=scores_svm.mean(axis=0),size=10,color='red')
@@ -114,7 +115,7 @@ def predict_obesity():
   p2.text(2.7,.51,['Drop access'],text_font_size='10pt')
   p2.text(3.6,.51,['Drop availability'], text_font_size='10pt')
 
-  p = gridplot([[p1],[p2]])
+  p = gridplot([[p1,p2]])
   script, div = components(p,CDN)
   return render_template('myplot.html', page_title=page_title, script=script, div=div)
 
@@ -122,38 +123,6 @@ def predict_obesity():
 @app.route('/food_choice')
 def food_choice():
   page_title = "Food choice"
-#  xl_file = 'DataDownload.xls'
-#  data = pd.ExcelFile(xl_file)
-#  sheet_names = data.sheet_names
-#  all_dfs = {sheet: data.parse(sheet) for sheet in sheet_names}
-
-#  restaurants_df = all_dfs['RESTAURANTS']
-#  restaurants_df = restaurants_df[['State','County','PC_FFRSALES07','PC_FSRSALES07']]
-#  restaurants_df.columns = ['State','County','Per Capita Fast Food','Per Capita Full Service']
-  
-#  socioec_df = all_dfs['SOCIOECONOMIC']
-#  socioec_df = socioec_df[['State','County','POVRATE10']]
-#  health_df = all_dfs['HEALTH']
-#  health_df = health_df[['State','County','PCT_OBESE_ADULTS10']]
-#  health_se_df = pd.merge(socioec_df,health_df,on=['State','County'])
-#  health_se_df.columns = ['State','County','Poverty Rate','Adult Obesity Rate']
-
-#  insec_df = all_dfs['INSECURITY']
-#  insec_df = insec_df[['State','County','FOODINSEC_10_12']]
-#  insec_df.columns = ['State','County','PercInsecure']
-
-#  insecRes_df = pd.merge(insec_df,restaurants_df, on=['State','County'])
-#  meals_df = pd.merge(health_se_df,insecRes_df,on = ['State','County'])
-#  meals_df['Meals FF'] = meals_df['Per Capita Fast Food']/6.0
-#  meals_df['Meals FS'] = meals_df['Per Capita Full Service']/12.0
-
-#  meals_df['Poverty Rate']=[x  if any([type(x) == int,type(x)==float]) else np.nan for x in meals_df['Poverty Rate']]
-#  meals_df['Poverty Rate'] = np.float64(meals_df['Poverty Rate'])
-
-#  stateMeans = meals_df.groupby('State').mean()
-#  stateMeans['percentFF'] = 100*stateMeans['Meals FF']/(stateMeans['Meals FF'] + stateMeans['Meals FS'])
-
-#  data = ColumnDataSource(data=dict(percentFF = stateMeans['percentFF'],povRate = stateMeans['Poverty Rate'],state = stateMeans.index))
   use_data2 = pickle.load(open('fig4bdata.p','rb'))
     
   hover = HoverTool(tooltips = [('State','@state')])
@@ -161,7 +130,7 @@ def food_choice():
       title = 'More meals "out" at fast food restaurants correlates with higher obesity',
       title_text_font_size='12pt', 
       x_axis_label = 'Percent of meals "out" at fast food',
-      y_axis_label = 'Obesity Rate',tools=[hover])
+      y_axis_label = 'Obesity Rate',tools=[hover],plot_width=500,plot_height=400)
   plot2.scatter('percentFF','obesityRate',size=10,source = use_data2)
 
   script, div = components(plot2,CDN)
